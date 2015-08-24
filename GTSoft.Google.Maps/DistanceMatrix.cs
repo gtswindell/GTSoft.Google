8bf307b87efc25741596a97c34bcaf2d5302f079
Glen Swindell <glen.swindell@tnt.com>
2015-08-24 15:34:11 +0100

    Initial Entry

---
+++ GTSoft.Google.Maps/DistanceMatrix.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GTSoft.Google.Maps
+{
+    public class DistanceMatrix : GoogleMapsAPIBase<DistanceMatrix.DistanceResult>
+    {
+
+        public class DataType
+        {
+            [JsonProperty(PropertyName="text")]
+            public string Text { get; set; }
+            [JsonProperty(PropertyName = "value")]
+            public int Value { get; set; }
+        }
+
+        public class Element
+        {
+            [JsonProperty(PropertyName = "distance")]
+            public DataType Distance { get; set; }
+            [JsonProperty(PropertyName = "duration")]
+            public DataType Duration { get; set; }
+            [JsonProperty(PropertyName = "status")]
+            public string Status { get; set; }
+        }
+
+        public class Row
+        {
+            [JsonProperty(PropertyName = "elements")]
+            public List<Element> Elements { get; set; }
+        }
+
+        public class DistanceResult
+        {
+            [JsonProperty(PropertyName = "destination_addresses")]
+            public List<string> DestinationAddresses { get; set; }
+            [JsonProperty(PropertyName = "origin_addresses")]
+            public List<string> OriginAddresses { get; set; }
+            [JsonProperty(PropertyName = "rows")]
+            public List<Row> Rows { get; set; }
+            [JsonProperty(PropertyName = "status")]
+            public string Status { get; set; }
+        }
+
+        public DistanceMatrix(string apiKey)
+            : base(apiKey)
+        {
+
+        }
+
+        protected override string Url
+        {
+            get
+            {
+                string url = BaseUri + "distancematrix/json?key=" + APIKey;
+
+                return url;
+            }
+        }
+
+        protected override string APIName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+    }
+}
---
+++ GTSoft.Google.Maps/GoogleMapsAPIBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTSoft.Google.Maps
+{
+    public abstract class GoogleMapsAPIBase<T>
+    {
+        const string baseUrl = @"https://maps.googleapis.com/maps/api/";
+        //public enum OutputType { JSON, XML };
+        public enum UnitType { Metric, Imperial };
+
+        protected GoogleMapsAPIBase(string apiKey)
+        {
+            this.APIKey = apiKey;
+            //Output = OutputType.JSON;
+            Unit = UnitType.Metric;
+        }
+
+        public T GetData()
+        {
+            string url = Url;
+            return default(T);
+        }
+
+        protected string BaseUri {
+            get
+            {
+                return baseUrl;
+            }
+        }
+        protected abstract string APIName { get; }
+        protected abstract string Url{ get; }
+        
+        public string APIKey { get; private set; }
+        public UnitType Unit { get; set; }
+    }
+}
